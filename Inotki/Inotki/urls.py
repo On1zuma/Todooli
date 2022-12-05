8b1b6c3a1f7cfec25d0a9e2e5b9d7a5ffd05e5cf
On1zuma <wassi.mess@hotmail.com>
2022-12-05 19:49:44 +0100

    feat : User registration

--- Inotki/Inotki/urls.py
+++ Inotki/Inotki/urls.py
@@ -24,6 +24,7 @@ from tasks.views.tasks.task_update import TaskUpdate
 
 from tasks.views.users.user_login import UserLoginView
 from django.contrib.auth.views import LogoutView
+from tasks.views.users.user_register import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,6 +32,7 @@ urlpatterns = [
     #Users
     path('user/login/', UserLoginView.as_view(), name='login'),
     path('user/logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('user/register/', RegisterView.as_view(), name='register'),
 
     # Tasks
     path('', TaskList.as_view(), name='tasks'),
