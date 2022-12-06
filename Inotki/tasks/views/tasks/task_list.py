e5bb4b92de84b56a2bc5db3fd9cd3c03e6d9ce61
On1zuma <wassi.mess@hotmail.com>
2022-12-06 21:52:39 +0100

    feat : start of an admin interface with all the tasks

--- Inotki/tasks/views/tasks/task_list.py
+++ Inotki/tasks/views/tasks/task_list.py
@@ -11,8 +11,12 @@ class TaskList(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(complete=False).count
+        if self.request.user.is_staff:
+            context['admin'] = 'Admin dashboard'
+        #if the user is not an admin, then we filter the data
+        if not self.request.user.is_staff:
+            context['tasks'] = context['tasks'].filter(user=self.request.user)
+            context['count'] = context['tasks'].filter(complete=False).count
 
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
