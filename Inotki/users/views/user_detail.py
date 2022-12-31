204d742ffd874e2f1de526d60f718dc1493d56e7
On1zuma <wassi.mess@hotmail.com>
2022-12-31 23:49:56 +0100

    feat: Implementation of Private Profile option: True/False

--- Inotki/users/views/user_detail.py
+++ Inotki/users/views/user_detail.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.models import User
 from django.views.generic import DetailView
-
 from tasks.models.task import Task
-
+from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -10,3 +9,15 @@ class UserDetail(LoginRequiredMixin, DetailView):
     model = User
     template_name = "base/user_detail.html"
     context_object_name = 'user_profile'
+
+    def get(self, request, *args, **kwargs):
+        user_profile = User.objects.get(pk=kwargs['pk'])
+
+        # We check if the user is an admin
+        if self.request.user.is_staff:
+            return super().get(request, *args, **kwargs)
+
+        # We check if the user is trying to edit their own data
+        if user_profile != self.request.user and user_profile.option.private_profile == True:
+            return redirect('tasks')  # TODO: 404 page
+        return super().get(request, *args, **kwargs)
