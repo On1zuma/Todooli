a3a066919bc20f34974f8fa7286fb495c7cdfe37
On1zuma <wassi.mess@hotmail.com>
2022-12-31 22:22:39 +0100

    feat: new table option for user

---
+++ Inotki/users/models/option.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Option(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    show_completed_task = models.BooleanField(default=True)
+    instant_deletion = models.BooleanField(default=False)
+    email_notification = models.BooleanField(default=True)
+    dark_mode = models.BooleanField(default=False)
+    private_profile = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user.username} option'
