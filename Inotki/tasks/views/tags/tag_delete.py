5a90b642cc7c9db4f321f438bff7a18b67d08f0f
On1zuma <wassi.mess@hotmail.com>
2022-12-31 13:25:31 +0100

    feat: tags edit + delete

--- Inotki/tasks/views/tags/tag_delete.py
+++ Inotki/tasks/views/tags/tag_delete.py
@@ -1,36 +1,37 @@
 from django.shortcuts import redirect
 from django.views.generic import DeleteView
 
+from tasks.models.tag import Tag
 from tasks.models.task import Task
 from django.urls import reverse
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class TaskDelete(LoginRequiredMixin, DeleteView):
-    model = Task
-    context_object_name = 'task'
-    template_name = "tasks/task_delete.html"
+class TagDelete(LoginRequiredMixin, DeleteView):
+    model = Tag
+    context_object_name = 'tag'
+    template_name = "tags/tag_delete.html"
 
     def get(self, request, *args, **kwargs):
-        task = Task.objects.get(pk=kwargs['pk'])
+        tag = Tag.objects.get(pk=kwargs['pk'])
         if self.request.user.is_staff:
             return super().get(request, *args, **kwargs)
 
-        if task.user != self.request.user:
-            return redirect('tasks')  # TODO: faire une page 404 avec un message ou des flash messages
+        if tag.user != self.request.user:
+            return redirect('tags')  # TODO: faire une page 404 avec un message ou des flash messages
         return super().get(request, *args, **kwargs)
 
     def form_valid(self, form):
-        task = Task.objects.get(pk=self.object.id)
+        tag = Tag.objects.get(pk=self.object.id)
 
         if self.request.user.is_staff:
             return super().form_valid(form)
 
-        if task.user != self.request.user:
+        if tag.user != self.request.user:
             return super().form_invalid(form)
 
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse('tasks')
+        return reverse('tags')
---
+++ Inotki/tasks/views/tags/tag_update.py
@@ -0,0 +1,69 @@
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import UpdateView
+
+from tasks.form.date_input import DateInputForm
+from tasks.models.tag import Tag
+from tasks.models.task import Task
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class TagUpdate(LoginRequiredMixin, UpdateView):
+    model = Tag
+    template_name = "tags/tag_form.html"
+    fields = ['tag_name']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_staff:
+            context['admin'] = 'Admin'
+        return context
+
+    def get(self, request, *args, **kwargs):
+        tag = Tag.objects.get(pk=kwargs['pk'])
+
+        # We check if the user is an admin
+        if self.request.user.is_staff:
+            return super().get(request, *args, **kwargs)
+
+        # We check if the user is trying to edit their own data
+        if tag.user != self.request.user:
+            # flash message: messages.add
+            return redirect('tags')
+        return super().get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        tag = Tag.objects.get(pk=self.object.id)
+
+        # if staff user and the data don't is not assigned to any user (normally not possible)
+        if self.request.user.is_staff and self.object.user is None:
+            form.instance.user = self.request.user
+            messages.success(self.request, f'Succes, your tag has been updated', 'success')
+            return super().form_valid(form)
+
+        # if staff user and the data does not belong to the admin
+        if self.request.user.is_staff:
+            messages.success(self.request, f'Succes, your tag has been updated', 'success')
+            return super().form_valid(form)
+
+        # if the task is not assigned to the user who is editing the task
+        if form.instance.user is not None:
+            if tag.user != form.instance.user or tag.user != self.request.user:
+                form.add_error(None, "You can't do that")  # add message to the user that are not supposed to do that
+                messages.success(self.request, f'You are not allowed to do that', 'danger')
+                return super().form_invalid(form)
+
+        if tag.user != self.request.user:
+            form.add_error(None, "You can't do that")  # add message to the user that are not supposed to do that
+            messages.success(self.request, f'You are not allowed to do that', 'danger')
+            return super().form_invalid(form)
+
+        # name of passed object   #name of the user session
+        form.instance.user = self.request.user
+        messages.success(self.request, f'Succes, your tag has been updated', 'success')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('tags')
