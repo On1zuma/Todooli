0f1111348e395ff5427c2cf587fc3e0fe4eeb99c
On1zuma <wassi.mess@hotmail.com>
2023-01-08 16:39:53 +0100

    feat: update flash messages

--- Inotki/static/js/app.js
+++ Inotki/static/js/app.js
@@ -1,6 +1,39 @@
+//burger
 let toggle = document.querySelector('.toggle');
 let body = document.querySelector('body');
 
 toggle.addEventListener('click', function () {
     body.classList.toggle('open');
 })
+
+//flash message
+const button = document.querySelector("button"),
+  toast = document.querySelector(".toast");
+(closeIcon = document.querySelector(".close")),
+  (progress = document.querySelector(".progress"));
+
+let timer1, timer2;
+
+if (document.getElementsByClassName('alert')) {
+  toast.classList.add("active");
+  progress.classList.add("active");
+
+  timer1 = setTimeout(() => {
+    toast.classList.remove("active");
+  }, 5000); //1s = 1000 milliseconds
+
+  timer2 = setTimeout(() => {
+    progress.classList.remove("active");
+  }, 5300);
+};
+
+closeIcon.addEventListener("click", () => {
+  toast.classList.remove("active");
+
+  setTimeout(() => {
+    progress.classList.remove("active");
+  }, 300);
+
+  clearTimeout(timer1);
+  clearTimeout(timer2);
+});
