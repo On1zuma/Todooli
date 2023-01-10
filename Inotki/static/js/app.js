ada4060c582607070219475c2799ee3522e4fee6
On1zuma <wassi.mess@hotmail.com>
2023-01-10 20:33:25 +0100

    fix: disable "js" if flash message is not set

--- Inotki/static/js/app.js
+++ Inotki/static/js/app.js
@@ -19,32 +19,34 @@ toggle.addEventListener('click', function () {
 //flash message
 const button = document.querySelector("button"),
     toast = document.querySelector(".toast");
-(closeIcon = document.querySelector(".close")),
-    (progress = document.querySelector(".progress"));
+if(toast){
+  (closeIcon = document.querySelector(".close")),
+      (progress = document.querySelector(".progress"));
 
-let timer1, timer2;
+  let timer1, timer2;
 
-if (document.getElementsByClassName('alert')) {
-  toast.classList.add("active");
-  progress.classList.add("active");
+  if (document.getElementsByClassName('alert')) {
+    toast.classList.add("active");
+    progress.classList.add("active");
 
-  timer1 = setTimeout(() => {
+    timer1 = setTimeout(() => {
+      toast.classList.remove("active");
+    }, 5000); //1s = 1000 milliseconds
+
+    timer2 = setTimeout(() => {
+      progress.classList.remove("active");
+    }, 5300);
+  };
+
+  closeIcon.addEventListener("click", () => {
     toast.classList.remove("active");
-  }, 5000); //1s = 1000 milliseconds
 
-  timer2 = setTimeout(() => {
-    progress.classList.remove("active");
-  }, 5300);
-};
+    setTimeout(() => {
+      progress.classList.remove("active");
+    }, 300);
 
-closeIcon.addEventListener("click", () => {
-  toast.classList.remove("active");
-
-  setTimeout(() => {
-    progress.classList.remove("active");
-  }, 300);
-
-  clearTimeout(timer1);
-  clearTimeout(timer2);
-});
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+  });
+}
 
